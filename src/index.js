263927f5a02f6ef0aa690f111291259cd9102d93
ramp4 <rampatv13@gmail.com>
2019-03-24 02:00:28 +0300

    feat: add function which exchange money

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let inputError = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    if (currency <= 0) 
+        return {}; 
+    if (currency > 10000) 
+        return inputError;
+    let money = currency;
+    console.log(money);    
+    const coinsCost = [50,25,10,5,1];
+    let coinsName = ["H","Q","D","N","P"];
+    let coinsCounter = [0,0,0,0,0];
+    let stopIndex = 0 ;
+    let startIndex = 0;
+    while ( money > 0) {
+        
+        for (i = 0; i <= 4; i++) {
+            while (coinsCost[i] <= money) {
+            money -= coinsCost[i];
+            coinsCounter[i]++;
+            }
+            
+            if ( money == 0) break; 
+        }  
+    }
+       
+    result = {};
+    for ( i = 0; i <= 4; i++) {
+        if ( coinsCounter[i] > 0 ) 
+        result[coinsName[i]] = coinsCounter[i];
+    }
+    
+    return result;
+    
+    
+    
 }
